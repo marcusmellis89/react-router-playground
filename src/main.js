1c9f7d699a9369c1296f0d80df660300a03e3afd
marcusmellis89 <m.m.ellis89@gmail.com>
2016-08-22 13:39:33 -0700

    [routes]namedComponents

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './routes/routeParams';
+import App from './routes/nameComponents';
 ReactDOM.render(<App />,document.getElementById('app'));
---
+++ src/routes/nameComponents.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
+
+const Home = () => <h1>Home</h1>
+const HomeBody = () => <div>This is HomeBody</div>;
+const Other = () => <h1>Other</h1>;
+const OtherBody = () => <div>This is the other body</div>
+
+const Container = (props) => <div>{props.header}{props.body}<Links/></div>
+
+const Links = () => 
+	<nav>
+		<Link to='/'>Root</Link><br/>
+		<Link to='/other'>Other</Link><br/>
+	</nav>
+
+
+//Basic Route Setup
+class App extends React.Component {
+	render(){
+		return (
+			<Router history={ hashHistory }>
+				<Route path="/" component={Container}>
+					<IndexRoute components={{header: Home, body:HomeBody}}></IndexRoute>
+					<Route path="other" components={{header: Other, body:OtherBody}}></Route>
+				</Route>
+			</Router>
+		);
+	}
+}
+
+export default App;
